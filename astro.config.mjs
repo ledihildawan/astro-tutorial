58fcd799ecf0a189ebd5120e31ec0ddc7bf2b290
ledihildawan <ledi.hildawan@salt.co.id>
2025-12-11 13:36:47 +0700

    deploy to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   site: 'http://localhost:4321',
   integrations: [preact()],
+  adapter: netlify(),
 });
